8fa58f2d5a6137e558247d613329395a89d4b499
nithin <nithinreddy1572@gmail.com>
2023-02-26 16:49:49 +0530

    added table

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use(express.static("frontend"));
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/frontend/index.html");
 });
-
 mongoose.set('strictQuery',true);
 mongoose.connect(process.env.MONGO_CONNECTION_STRING,async function(err){
     if(err) console.log(err);
