76ac321e545bb5f06ef5eb644a29ff1e9632a762
nithin <nithinreddy1572@gmail.com>
2023-02-26 14:14:47 +0530

    adding mongoaDB to project

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
 const app = express();
-
+dotenv.config();
 app.use(express.static("frontend"));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/frontend/index.html");
 });
 
-app.listen(3000,function(){
-    console.log("server runnning on http://localhost:3000");
-});
+mongoose.set('strictQuery',true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,function(err){
+    if(err) console.log(err);
+    else{
+        console.log("DB connection successfull");
+        app.listen(3000,function(){
+            console.log("server runnning on http://localhost:3000");
+        });
+    }
+})
