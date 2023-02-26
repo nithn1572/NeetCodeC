a4514ddbbe944d3ce515bc6c3af42b127a8e0c80
nithin <nithinreddy1572@gmail.com>
2023-02-26 15:10:59 +0530

    added update

--- backend/lib/courseLib.js
+++ backend/lib/courseLib.js
@@ -3,6 +3,13 @@ const courseModel = require("../models/courseModel.js");
 module.exports.getAllCourses = async function(){
     return await courseModel.find({});
 }
+module.exports.createCourse = async function(courseInput){
+    const course = new courseModel(courseInput);
+    await course.save();
+}
+module.exports.updateCourse = async function(courseId, updatedInput){
+    await courseModel.findOneAndUpdate({_id:courseId},updatedInput);
+}
 module.exports.createFirstCourse = async function(){
     const courses  = await courseModel.find({});
     // const courses = this.getAllCourses();
